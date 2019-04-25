f737948b070debe424571fa7dcdd794a56ade8c1
thytran1 <thytran@email.arizona.edu>
2019-04-24 17:17:28 -0700

    Add files via upload

--- prototype/javascript/paypage.js
+++ prototype/javascript/paypage.js
@@ -2,6 +2,9 @@
 // Setting up the Document
 document.addEventListener("DOMContentLoaded", function(event) {
     var phoneDive = phoneDiv("phoneNum");
+    var phoneButton = textReceipt("phone");
+    var emailButton = emailReceipt("email");
+    var printButton = printReceipt("print");
 });
 
 
@@ -14,4 +17,28 @@ var phoneDiv = function(divElementId) {
         _element.innerHTML = phoneNum;
         _element.style.display = "block";
     }
+}
+
+var textReceipt = function(divElementId) {
+	 var _element = document.getElementById(divElementId);
+
+    _element.addEventListener("click", function() {
+        window.alert("A text has been sent with a link to your receipt. Thank you!");
+    });
+}
+
+var emailReceipt = function(divElementId) {
+	 var _element = document.getElementById(divElementId);
+
+    _element.addEventListener("click", function() {
+        window.alert("An email has been sent with a link to your receipt. Thank you!");
+    });
+}
+
+var printReceipt = function(divElementId) {
+	 var _element = document.getElementById(divElementId);
+
+    _element.addEventListener("click", function() {
+        window.alert("Your receipt is being printed; a waiter will bring it to you shortly. Thank you!");
+    });
 }
